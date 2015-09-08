dbb88a34c730bf50616abe15e6c816e6dfb31fb0
Charles Lai <cjl223@cornell.edu>
2015-09-08 12:48:21 -0400

    add flask skeleton

--- MONGO/server.py
+++ MONGO/server.py
@@ -1,1 +1,9 @@
-import flask
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Index page of Cornell NBA Prediction Project API."
+
+if __name__ == "__main__":
+    app.run()
