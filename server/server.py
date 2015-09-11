83774c925551b9f9b7a8d9f06005068ef900103c
Charles Lai <cjl223@cornell.edu>
2015-09-11 17:13:14 -0400

    starting skeleton for API

--- server/server.py
+++ server/server.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
     return "Index page of Cornell NBA Prediction Project API."
 
+@app.route("/stats")
+def get_stats():
+    return "Get stats"
+
 if __name__ == "__main__":
     app.run()
