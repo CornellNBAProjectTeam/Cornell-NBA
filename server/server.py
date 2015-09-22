3ea25347bc761dcd7a918ca1c979c0b0533a34b7
Charles Lai <cjl223@cornell.edu>
2015-09-22 01:25:22 -0400

    alter main function for server for heroku

--- server/server.py
+++ server/server.py
@@ -10,4 +10,5 @@ def get_stats():
     return "Get stats"
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
